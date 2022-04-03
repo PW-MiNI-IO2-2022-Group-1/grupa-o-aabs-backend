f9683cb7dd33ebd8487e2b042596191df0e6047c
adamczykt <73478789+adamczykt@users.noreply.github.com>
2022-04-03 15:43:00 +0200

    Release 0.0.1

---
+++ src/test/java/com/example/SOPSbackend/SopsBackendApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.SOPSbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class SopsBackendApplicationTests {
+
+	@Test
+	void contextLoads() { }
+
+}
