2ff2e843f60c31cc571b7873e4259c5722919a2a
Filip Spychala <filipspych@gmail.com>
2022-04-14 03:15:38 +0200

    Add edit doctor endpoint. IO2-118

--- src/main/java/com/example/SOPSbackend/controller/AdminController.java
+++ src/main/java/com/example/SOPSbackend/controller/AdminController.java
@@ -1,14 +1,16 @@
 package com.example.SOPSbackend.controller;
 
+import com.example.SOPSbackend.dto.BasicUserWithoutPasswordDto;
+import com.example.SOPSbackend.dto.EditDoctorDto;
 import com.example.SOPSbackend.model.DoctorEntity;
-import com.example.SOPSbackend.response.BasicUserResponse;
-import com.example.SOPSbackend.response.ErrorResponseBody;
+import com.example.SOPSbackend.response.BasicUserOkResponse;
+import com.example.SOPSbackend.response.NotFoundResponse;
 import com.example.SOPSbackend.response.PaginatedResponse;
 import com.example.SOPSbackend.service.AdminService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
 @RestController
@@ -29,20 +31,29 @@ public class AdminController {
     public ResponseEntity<Object> showDoctors(@RequestParam Optional<Integer> page) {
         return ResponseEntity.ok(new PaginatedResponse<>(
                 adminService.getAllDoctors(page.orElse(0))
-                        .map(BasicUserResponse::new)
+                        .map(BasicUserWithoutPasswordDto::new)
                 )
         );
+    }
+
+    @PutMapping("doctors/{doctorId}")
+    public ResponseEntity<Object> editDoctor(@PathVariable String doctorId, @RequestBody EditDoctorDto editDoctor) {
+        try {
+            return new BasicUserOkResponse(adminService.updateDoctor(doctorId, editDoctor));
+        } catch (EntityNotFoundException e) {
+            return new NotFoundResponse();
+        }
     }
 
     @GetMapping("doctors/{doctorId}")
     public ResponseEntity<Object> getSingleDoctor(@PathVariable String doctorId) {
         Optional<DoctorEntity> doctor = adminService.getDoctor(Long.parseLong(doctorId));
-        if (doctor.isPresent()) return ResponseEntity.ok(new BasicUserResponse(doctor.get())); // TODO: should we have better exception handling? (This could throw NumberFormatException causing a 500 response)
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponseBody());
+        if (doctor.isPresent()) return new BasicUserOkResponse(doctor.get()); // TODO: should we have better exception handling? (This could throw NumberFormatException causing a 500 response)
+        return new NotFoundResponse();
     }
 
     @PostMapping("doctors")
     public ResponseEntity<Object> createDoctor(@RequestBody DoctorEntity doctor) {
-        return ResponseEntity.ok().body(new BasicUserResponse(adminService.addDoctor(doctor)));
+        return new BasicUserOkResponse(adminService.addDoctor(doctor));
     }
 }
