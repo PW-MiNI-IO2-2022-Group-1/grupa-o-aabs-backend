35ade87a679850b68a675943ac8e2268e768517d
VI0L3TF0X <mattex15243@gmail.com>
2022-12-01 22:06:34 +0100

    tree ready as a blueprint

--- UnrealEngine/FPSTest/Source/FPSTest/Private/GenericTree.cpp
+++ UnrealEngine/FPSTest/Source/FPSTest/Private/GenericTree.cpp
@@ -6,21 +6,34 @@
 #include "Math/RandomStream.h"
 #include "UObject/UObjectGlobals.h"
 
+FTreeComponentStruct::FTreeComponentStruct() {
+	Height = 0;
+	InitialSegmentSize = 20;
+	HorizontalStretch = 1;
+	Material = nullptr;
+	StaticMesh = nullptr;
+	Bounds = FVector2D(20, 100);
+	Offset = FVector2D(1,1);
+}
+
 // Sets default values
 AGenericTree::AGenericTree()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	Seed = 0;
 	PrimaryActorTick.bCanEverTick = false;
-	TrunkSegmentSize = 20.0;
-	TreetopSegmentSize = 100.0;
-	
-	auto cubeMesh = ConstructorHelpers::FObjectFinder<UStaticMesh>(TEXT("StaticMesh'/Engine/BasicShapes/Cube.Cube'")).Object;
-	auto cubeMaterial = LoadObject<UMaterial>(nullptr, TEXT("/Game/StarterContent/Materials/M_Wood_Walnut"));
-	Root = NewObject<UStaticMeshComponent>(this, TEXT("Root"));
-	SetRootComponent(Root);
-	Root->SetStaticMesh(cubeMesh);
-	Root->SetMaterial(0, cubeMaterial);
-	Root->SetMobility(EComponentMobility::Static);
+	Trunk.SegmentSize = 20.0;
+	Trunk.RotationFactor = 15;
+	Trunk.ScalingFactor = 0.05;
+	Treetop.SegmentSize = 100.0;
+	Treetop.Offset = FVector2D(0.3, 0.7);
+	Treetop.RotationFactor = 5;
+	Treetop.ScalingFactor = 0.1;
+	Trunk.Bounds = FVector2D(100, 200);
+	Treetop.Bounds = FVector2D(150, 300);
+	// TrunkStaticMesh = LoadObject<UStaticMesh>(nullptr, TEXT("/Game/StarterContent/Shapes/Shape_Cube"));
+	// TrunkMaterial = LoadObject<UMaterial>(nullptr, TEXT("/Game/StarterContent/Materials/M_Wood_Walnut"));
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
 }
 
 void AGenericTree::Initialize(
@@ -33,17 +46,10 @@ void AGenericTree::Initialize(
 	int maxTreetopHeight)
 {
 	
-	FRandomStream stream = FRandomStream(seed + GetActorLocation().X + GetActorLocation().Y + GetActorLocation().Z);
-	TrunkHeight = stream.RandRange(minTrunkHeight, maxTrunkHeight);
-	TreetopHeight = stream.RandRange(minTreetopHeight, maxTreetopHeight);
-	double trunkSegmentHeight = Root->GetStaticMesh()->GetBoundingBox().GetSize().Z;
-	TrunkSegmentSize = TrunkHeight / round(TrunkHeight / TrunkSegmentSize); // actual size -> nearest value to segment size so height is precise
-	auto trunkScaleVector =  FVector(TrunkSegmentSize / trunkSegmentHeight);
-	Root->SetWorldScale3D(trunkScaleVector);
-	Root->SetRelativeScale3D(trunkScaleVector);
-
-	RenderTrunk();
-	RenderTreetop();
+	// TrunkStaticMesh = trunkStaticMesh;
+	// TrunkMaterial = trunkMaterial;
+	Trunk.Bounds = FVector2D(minTrunkHeight, maxTrunkHeight);
+	Treetop.Bounds = FVector2D(minTreetopHeight, maxTreetopHeight);
 }
 
 // Called when the game starts or when spawned
@@ -54,33 +60,33 @@ void AGenericTree::BeginPlay()
 
 void AGenericTree::RenderTrunk()
 {
-	FVector pos = GetActorLocation();
-	double currentHeight = TrunkSegmentSize;
+	//FVector pos = GetActorLocation();
+	//double currentHeight = TrunkSegmentSize;
 
-	FActorSpawnParameters spawnParams = FActorSpawnParameters();
-	spawnParams.Template = Cast<AActor>(Root);
+	//FActorSpawnParameters spawnParams = FActorSpawnParameters();
+	//spawnParams.Template = Cast<AActor>(Root);
 
-	while (currentHeight < TrunkHeight) {
-		auto newTrunkSegment = DuplicateObject<UStaticMeshComponent>(Root, this);
+	//while (currentHeight < TrunkHeight) {
+	//	auto newTrunkSegment = DuplicateObject<UStaticMeshComponent>(Root, this);
 
-		if (newTrunkSegment != nullptr)
-		{
-			FVector newLocation = FVector(
-				pos.X,
-				pos.Y,
-				pos.Z + currentHeight
-			);
+	//	if (newTrunkSegment != nullptr)
+	//	{
+	//		FVector newLocation = FVector(
+	//			pos.X,
+	//			pos.Y,
+	//			pos.Z + currentHeight
+	//		);
 
-			if (GEngine != NULL && !newLocation.IsZero()) {
-				// GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString(TEXT("Spawn tree trunk segment at ")).Append(newLocation.ToCompactString()));
-			}
-			newTrunkSegment->SetWorldLocation(newLocation);
-			newTrunkSegment->RegisterComponent();
-			Trunk.Add(newTrunkSegment);
-			currentHeight += TrunkSegmentSize;
-		}
-		
-	}
+	//		if (GEngine != NULL && !newLocation.IsZero()) {
+	//			// GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString(TEXT("Spawn tree trunk segment at ")).Append(newLocation.ToCompactString()));
+	//		}
+	//		newTrunkSegment->SetWorldLocation(newLocation);
+	//		newTrunkSegment->RegisterComponent();
+	//		Trunk.Add(newTrunkSegment);
+	//		currentHeight += TrunkSegmentSize;
+	//	}
+	//	
+	//}
 }
 
 void AGenericTree::RenderTreetop()
--- UnrealEngine/FPSTest/Source/FPSTest/Public/GenericTree.h
+++ UnrealEngine/FPSTest/Source/FPSTest/Public/GenericTree.h
@@ -6,6 +6,44 @@
 #include "GameFramework/Actor.h"
 #include "GenericTree.generated.h"
 
+USTRUCT(BlueprintType)
+struct FTreeComponentStruct
+{
+	GENERATED_BODY()
+public:
+	FTreeComponentStruct();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tree Struct", meta = (ClampMin = "1"))
+		double Height;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tree Struct", meta = (ClampMin = "1"))
+		double InitialSegmentSize;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tree Struct", meta = (ClampMin = "1"))
+		double HorizontalStretch;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tree Struct", meta = (ClampMin = "1"))
+		double SegmentSize;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tree Struct", meta = (ClampMin = "1"))
+		FVector2D Offset;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tree Struct", meta = (ClampMin = "1"))
+		double RotationFactor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tree Struct", meta = (ClampMin = "1"))
+		double ScalingFactor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tree Struct")
+		FVector2D Bounds;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tree Struct")
+		UStaticMesh* StaticMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tree Struct")
+		UMaterial* Material;
+};
+
 UCLASS()
 class FPSTEST_API AGenericTree : public AActor
 {
@@ -13,11 +51,7 @@ class FPSTEST_API AGenericTree : public AActor
 	
 public:	
 	// Sets default values for this actor's properties
-	UPROPERTY(EditAnywhere, meta = (ClampMin = "1"))
-		double TrunkSegmentSize;
 
-	UPROPERTY(EditAnywhere, meta = (ClampMin = "1"))
-		double TreetopSegmentSize;
 	AGenericTree();
 	virtual void Initialize(
 		int seed,
@@ -28,21 +62,23 @@ public:
 		int minTreetopHeight = 40,
 		int maxTreetopHeight = 400
 	);
-
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 	virtual void RenderTrunk();
 	virtual void RenderTreetop();
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tree variables")
+		FTreeComponentStruct Trunk;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tree variables", meta = (ClampMin = "1"))
+		FTreeComponentStruct Treetop;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Tree variables")
+		FRandomStream Stream;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tree variables")
+		int Seed;
 private:
-	unsigned int TrunkHeight;
-	unsigned int TreetopHeight;
-	UStaticMeshComponent* TrunkStaticMesh;
-	UStaticMeshComponent* TreeStaticMesh;
-	UStaticMeshComponent* Root;
-	TArray<UStaticMeshComponent*> Trunk;
-	TArray<UStaticMeshComponent*> Treetop;
 
 };
